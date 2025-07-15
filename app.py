82c5bd142d73c1744d1cd7be92ece81993a9ba5f
Mohammad Taha <107998043+taha775@users.noreply.github.com>
2025-07-16 01:33:26 +0500

    updated

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from snapchat_downloader import SnapchatDownloader
 from download_manager import DownloadManager
 from api_routes import create_api_routes
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -15,5 +16,9 @@ download_manager = DownloadManager(downloader)
 # Create API routes
 create_api_routes(app, downloader, download_manager)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
